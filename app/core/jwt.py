27e8df22d6b49b2f6178d3426d8f1a9110b323d8
Odhiambo <odhiambo123@users.noreply.github.com>
2026-01-20 03:01:20 -0800

    cleanup

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -1,24 +1,45 @@
 from datetime import datetime, timedelta, timezone
 from jose import jwt
-
 from app.core.config import settings
 
 
-def create_access_token(sub: str):
-    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    payload = {
-        "sub": sub,
-        "exp": expire,
-        "type": "access",
-    }
-    return jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + (
+        expires_delta
+        if expires_delta
+        else timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    to_encode.update(
+        {
+            "exp": expire,
+            "type": "access",
+        }
+    )
+
+    return jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM,
+    )
 
 
-def create_refresh_token(sub: str):
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+
     expire = datetime.now(timezone.utc) + timedelta(days=7)
-    payload = {
-        "sub": sub,
-        "exp": expire,
-        "type": "refresh",
-    }
-    return jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+    to_encode.update(
+        {
+            "exp": expire,
+            "type": "refresh",
+        }
+    )
+
+    return jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM,
+    )
